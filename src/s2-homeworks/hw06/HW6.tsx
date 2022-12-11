b68930dda5e8ff97ea536934c729990c5a46a717
petritsyn <andrey2343@yandex.ru>
2022-12-11 17:34:51 +0300

    hw6 fix ls bag

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -21,7 +21,7 @@ const HW6 = () => {
     const restore = () => {
         // делают студенты
         // ЗАСЕТАТЬ(restoreState<ТИПИЗАЦИЯ>(КЛЮЧ,''))
-        setValue(restoreState<string>('editable-span-value', ''))
+        setValue(restoreState<string>('hw6-editable-span-value', ''))
     }
 
     return (
